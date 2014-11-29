a54d9cca11fce5eaee6e5c2b36ab308185e38e3f
shirashiki <silvio.hirashiki@gmail.com>
2014-11-28 21:58:49 -0500

    completed day 7 unit 2

--- src/main/java/kiloboltgame/Robot.java
+++ src/main/java/kiloboltgame/Robot.java
@@ -1,6 +1,6 @@
 package kiloboltgame;
 
-import java.awt.Graphics;
+import java.util.ArrayList;
 
 public class Robot {
 
@@ -21,6 +21,7 @@ public class Robot {
 
 	private int speedX = 0;
 	private int speedY = 1;
+	private ArrayList<Projectile> projectiles = new ArrayList<Projectile>();
 
 	public void update() {
 
@@ -110,6 +111,11 @@ public class Robot {
 
 	}
 
+	public void shoot() {
+		Projectile p = new Projectile(centerX + 50, centerY - 25);
+		projectiles.add(p);
+	}
+
 	public int getCenterX() {
 		return centerX;
 	}
@@ -128,6 +134,10 @@ public class Robot {
 
 	public int getSpeedY() {
 		return speedY;
+	}
+
+	public ArrayList getProjectiles() {
+		return projectiles;
 	}
 
 	public void setCenterX(int centerX) {
--- src/main/java/kiloboltgame/StartingClass.java
+++ src/main/java/kiloboltgame/StartingClass.java
@@ -8,6 +8,7 @@ import java.awt.Image;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class StartingClass extends Applet implements Runnable, KeyListener {
 
@@ -74,17 +75,27 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 	public void run() {
 
 		while (true) {
-			
+
 			robot.update();
 			if (robot.isJumped()) {
 				currentSprite = characterJumped;
 			} else if (robot.isJumped() == false && robot.isDucked() == false) {
 				currentSprite = character;
 			}
-			
+
+			ArrayList<Projectile> projectiles = robot.getProjectiles();
+			for (int i = 0; i < projectiles.size(); i++) {
+				Projectile p = (Projectile) projectiles.get(i);
+				if (p.isVisible() == true) {
+					p.update();
+				} else {
+					projectiles.remove(i);
+				}
+			}
+
 			hb.update();
 			hb2.update();
-			
+
 			bg1.update();
 			bg2.update();
 
@@ -128,10 +139,17 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 		g.drawImage(background, bg1.getBgX(), bg1.getBgY(), this);
 		g.drawImage(background, bg2.getBgX(), bg2.getBgY(), this);
 
+		ArrayList<Projectile> projectiles = robot.getProjectiles();
+		for (int i = 0; i < projectiles.size(); i++) {
+			Projectile p = (Projectile) projectiles.get(i);
+			g.setColor(Color.YELLOW);
+			g.fillRect(p.getX(), p.getY(), 10, 5);
+		}
+
 		// draws character, in this case on top of background
 		g.drawImage(currentSprite, robot.getCenterX() - 61,
 				robot.getCenterY() - 63, this);
-		
+
 		g.drawImage(heliboy, hb.getCenterX() - 48, hb.getCenterY() - 48, this);
 		g.drawImage(heliboy, hb2.getCenterX() - 48, hb2.getCenterY() - 48, this);
 	}
@@ -164,6 +182,12 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 
 		case KeyEvent.VK_SPACE:
 			robot.jump();
+			break;
+
+		case KeyEvent.VK_CONTROL:
+			if (robot.isDucked() == false && robot.isJumped() == false) {
+				robot.shoot();
+			}
 			break;
 
 		}
