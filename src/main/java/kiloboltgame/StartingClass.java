2503758d264fc2cd991b5c5fe959fe820567c62b
shirashiki <silvio.hirashiki@gmail.com>
2014-11-28 20:59:29 -0500

    update

--- src/main/java/kiloboltgame/StartingClass.java
+++ src/main/java/kiloboltgame/StartingClass.java
@@ -12,8 +12,9 @@ import java.net.URL;
 public class StartingClass extends Applet implements Runnable, KeyListener {
 
 	private Robot robot;
-	private Image image, character, characterDown, 
-		characterJumped, currentSprite, background;
+	private Heliboy hb, hb2;
+	private Image image, character, characterDown, characterJumped,
+			currentSprite, background, heliboy;
 	private Graphics second;
 	private URL base;
 	private static Background bg1, bg2;
@@ -40,7 +41,7 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 		characterJumped = getImage(base, "data/jumped.png");
 		currentSprite = character;
 		background = getImage(base, "data/background.png");
-
+		heliboy = getImage(base, "data/heliboy.png");
 	}
 
 	@Override
@@ -48,6 +49,8 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 
 		bg1 = new Background(0, 0);
 		bg2 = new Background(2160, 0);
+		hb = new Heliboy(340, 360);
+		hb2 = new Heliboy(700, 360);
 		robot = new Robot();
 
 		Thread thread = new Thread(this);
@@ -71,12 +74,17 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 	public void run() {
 
 		while (true) {
+			
 			robot.update();
-			if (robot.isJumped()){
+			if (robot.isJumped()) {
 				currentSprite = characterJumped;
-				}else if (robot.isJumped() == false && robot.isDucked() == false){
+			} else if (robot.isJumped() == false && robot.isDucked() == false) {
 				currentSprite = character;
-				}
+			}
+			
+			hb.update();
+			hb2.update();
+			
 			bg1.update();
 			bg2.update();
 
@@ -121,7 +129,11 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 		g.drawImage(background, bg2.getBgX(), bg2.getBgY(), this);
 
 		// draws character, in this case on top of background
-		g.drawImage(currentSprite, robot.getCenterX() - 61, robot.getCenterY() - 63, this);
+		g.drawImage(currentSprite, robot.getCenterX() - 61,
+				robot.getCenterY() - 63, this);
+		
+		g.drawImage(heliboy, hb.getCenterX() - 48, hb.getCenterY() - 48, this);
+		g.drawImage(heliboy, hb2.getCenterX() - 48, hb2.getCenterY() - 48, this);
 	}
 
 	@Override
