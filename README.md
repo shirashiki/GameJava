d4cb266a01b9d03158613a2ef708c5ac8f03809c
shirashiki <silvio.hirashiki@gmail.com>
2014-11-28 19:06:07 -0500

    updated

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-GameJava
-========
+### GameJava
 
-Implementing  the game as in kilobolt tutorial
+Implementing  the game as in kilobolt tutorial. 
+
+
+#### References
+
+- [Kilobolt Game Tutorial](http://www.kilobolt.com/game-development-tutorial.html)
--- src/main/java/kiloboltgame/StartingClass.java
+++ src/main/java/kiloboltgame/StartingClass.java
@@ -3,30 +3,40 @@ package kiloboltgame;
 import java.applet.Applet;
 import java.awt.Color;
 import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.net.URL;
 
-public class StartingClass extends Applet implements Runnable{
-	
+public class StartingClass extends Applet implements Runnable, KeyListener {
+
 	private static final int MILI_INTERVAL = 17;
-	
+
+	private Robot robot;
+	private Image image, character;
+	private URL base;
+	private Graphics second;
+
 	@Override
 	public void init() {
-		super.init();
-		
+
 		setSize(800, 480);
 		setBackground(Color.BLACK);
-		
+
 		// takes focus
 		setFocusable(true);
 		Frame frame = (Frame) this.getParent().getParent();
-        frame.setTitle("Q-Bot Alpha");
+		frame.setTitle("Q-Bot Alpha");
 	}
 
 	@Override
 	public void start() {
-		super.start();
-		
-		   Thread thread = new Thread(this);
-		    thread.start();
+		robot = new Robot();
+
+		Thread thread = new Thread(this);
+		thread.start();
+
 	}
 
 	@Override
@@ -38,18 +48,105 @@ public class StartingClass extends Applet implements Runnable{
 	public void destroy() {
 		super.destroy();
 	}
-	
+
 	@Override
 	public void run() {
-		
-		while(true) {
+
+		while (true) {
 			repaint();
-			try{
+			try {
 				Thread.sleep(MILI_INTERVAL);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
-		
+
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		g.drawImage(character, robot.getCenterX() - 61,
+				robot.getCenterY() - 63, this);
+
+	}
+
+	/**
+	 * This implements double buffering in Applets
+	 */
+	@Override
+	public void update(Graphics g) {
+		if (image == null) {
+			image = createImage(this.getWidth(), this.getHeight());
+			second = image.getGraphics();
+		}
+
+		second.setColor(getBackground());
+		second.fillRect(0, 0, getWidth(), getHeight());
+		second.setColor(getForeground());
+		paint(second);
+
+		g.drawImage(image, 0, 0, this);
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_UP:
+			System.out.println("Move up");
+			break;
+
+		case KeyEvent.VK_DOWN:
+			System.out.println("Move down");
+			break;
+
+		case KeyEvent.VK_LEFT:
+			System.out.println("Move left");
+			break;
+
+		case KeyEvent.VK_RIGHT:
+			System.out.println("Move right");
+			break;
+
+		case KeyEvent.VK_SPACE:
+			System.out.println("Jump");
+			break;
+		}
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_UP:
+			System.out.println("Stop moving up");
+			break;
+
+		case KeyEvent.VK_DOWN:
+			System.out.println("Stop moving down");
+			break;
+
+		case KeyEvent.VK_LEFT:
+			System.out.println("Stop moving left");
+			break;
+
+		case KeyEvent.VK_RIGHT:
+			System.out.println("Stop moving right");
+			break;
+
+		case KeyEvent.VK_SPACE:
+			System.out.println("Stop jumping");
+			break;
+
+		}
+
 	}
 }
